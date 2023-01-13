0db7103a14d5df2b67a76f5cf7237430b9e7938d
tejasri354970 <101485828+tejasri354970@users.noreply.github.com>
2023-01-13 23:55:03 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h1 className="App-title">WELCOME TO REACT</h1>
          <p>
-          Hi There!
+          HI PRIYANKA
         </p>
         <a
           className="App-link"
